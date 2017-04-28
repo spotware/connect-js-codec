88ed4528a0c3ac092685fd7421615ffa2acfcf94
vspirin <vspirin@spotware.com>
2017-04-28 14:54:14 +0300

    remove adapter from codec

--- index.ts
+++ index.ts
@@ -1,16 +1,12 @@
-interface Adapter {
-    onData: (callback: (data: ArrayBuffer) => void) => void;
-}
-
 interface EncoderDecoder {
-    encode: (data: ArrayBuffer) => ArrayBuffer;
-    decode: (data: ArrayBuffer) => void;
-    registerDecodeHandler: (callback: (data: ArrayBuffer) => void) => void;
+    encode: (data: ArrayBuffer | Buffer) => ArrayBuffer | Buffer;
+    decode: (data: ArrayBuffer | Buffer) => void;
+    registerDecodeHandler: (callback: (data: ArrayBuffer | Buffer) => void) => void;
 }
 
 interface Protocol {
-    encode: (payloadType: number, payload: Object, clienMsgId: string) => ArrayBuffer;
-    decode: (data: ArrayBuffer) => Message;
+    encode: (payloadType: number, payload: Object, clienMsgId: string) => ArrayBuffer | Buffer;
+    decode: (data: ArrayBuffer | Buffer) => Message;
 }
 
 interface Message {
@@ -20,26 +16,27 @@ interface Message {
 }
 
 interface Codec {
-    encode: (payloadType: number, payload: Object, clientMsgId: string) => ArrayBuffer;
-    decode: (callback: (payloadType: number, payload: Object, clientMsgId: string) => void) => void;
+    encode: (payloadType: number, payload: Object, clientMsgId: string) => ArrayBuffer | Buffer;
+    decode: (data: ArrayBuffer | Buffer) => void;
+    subscribe: (callback: (payloadType: number, payload: Object, clienMsgId: string) => void) => void;
 }
 
-export function codec (adapter: Adapter, encoderDecoder: EncoderDecoder, protocol: Protocol): Codec {
+export function codec (encoderDecoder: EncoderDecoder, protocol: Protocol): Codec {
     return {
         encode: (payloadType, payload, clientMsgId) => {
             return encoderDecoder.encode(
                 protocol.encode(payloadType, payload, clientMsgId)
             );
         },
-        decode: (callback) => {
+        decode: (data) => {
+            encoderDecoder.decode(data);
+        },
+        subscribe: (callback) => {
             encoderDecoder.registerDecodeHandler((data) => {
                 const
                     message = protocol.decode(data);
 
                 callback(message.payloadType, message.payload, message.clientMsgId);
-            });
-            adapter.onData((data) => {
-                encoderDecoder.decode(data);
             });
         }
     };
